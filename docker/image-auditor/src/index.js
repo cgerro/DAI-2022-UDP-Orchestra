cf805af4a9dcc58425d4715ac6f762d259405993
leandro <leandro262009@gmail.com>
2023-01-23 15:54:59 +0100

    Added cool logs + UDP server on auditor

--- docker/image-auditor/src/index.js
+++ docker/image-auditor/src/index.js
@@ -1,14 +1,68 @@
+require('colors')
+const LogType = {
+	TCP: 'TCP',
+	UDP: 'UDP',
+};
+
+const getCurrentDateString = () => {
+	const d = new Date();
+	const year = d.getFullYear();
+	const month = (d.getMonth()+1).toString().padStart(2, '0');
+	const day = d.getDate().toString().padStart(2, '0');
+	const hours = d.getHours().toString().padStart(2, '0');;
+	const minutes = d.getMinutes().toString().padStart(2, '0');;
+	const seconds = d.getSeconds().toString().padStart(2, '0');;
+	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+const addLog = (type, message) => {
+	let msg = `${getCurrentDateString()} [${type}] ${message}`;
+	switch (type) {
+		case 'TCP': msg = msg.cyan; break;
+		case 'UDP': msg = msg.magenta; break;
+	}
+	console.log(msg);
+};
+
+// ---------- LOAD CONFIG ----------
+const config = require('../../config.js')
+const udpPort = config.PROTOCOL_PORT;
+const multicast_address = config.PROTOCOL_MULTICAST_ADDRESS;
+
+// ---------- UDP SERVER ----------
+const dgram = require('dgram')
+const udpSocket = dgram.createSocket('udp4');
+
+udpSocket.on('error', (err) => {
+	addLog(LogType.UDP, `Server error:\n${err.stack}`);
+	udpSocket.close();
+});
+
+udpSocket.on('message', (msg, rinfo) => {
+	addLog(LogType.UDP, `Server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+udpSocket.on('listening', () => {
+	const address = server.address();
+	addLog(LogType.UDP, `Started UDP server for auditor on ${address}:${udpPort}`);
+})
+
+udpSocket.bind(udpPort, () => {
+	udpSocket.addMembership(multicast_address);
+});
+
+// ---------- TCP SERVER ----------
 const Net = require('net');
-const port = 8080;
+const tcpPort = 2205;
 
 const server = new Net.Server();
-server.listen(port, function () {
-	console.log(`Started musician on port ${port}`);
+server.listen(tcpPort, function () {
+	addLog(LogType.TCP, `Started TCP server on port ${tcpPort}`);
 });
 
 // Listen to incoming request
 server.on('connection', function (socket) {
-	console.log('Connexion to client initiated');
+	addLog(LogType.TCP, 'Connexion to client initiated');
 
 	// Generate musicians array
 	// TODO
@@ -30,14 +84,14 @@ server.on('connection', function (socket) {
 
 	// Ignore client data
 	socket.on('data', function (chunk) {
-		console.log(`Data received from client, but ignored:\n${chunk.toString()}`);
+		addLog(LogType.TCP, `Data received from client, but ignored:\n${chunk.toString()}`);
 	});
 
 	socket.on('end', function () {
-		console.log('Connexion to client terminated');
+		addLog(LogType.TCP, 'Connexion to client terminated');
 	});
 
 	socket.on('error', function (err) {
-		console.log(`Error: ${err}`);
+		addLog(LogType.TCP, `Error: ${err}`);
 	});
 });
