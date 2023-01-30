3bfe5af0a74aa12618757e7dcb1de48467c5ef94
Céline Roger <roger.celine93@gmail.com>
2023-01-30 09:52:11 +0100

    modif du musicien

--- docker/image-musician/src/index.js
+++ docker/image-musician/src/index.js
@@ -1,22 +1,16 @@
-// 1 récupérer l'argument passer en paramètre
-
-// créer un socket udp
-
-// faire une fonction qui s'appelle chaque seconde et qui affiche un msg
-
-var args = process.argv.slice(2);
-console.log(args);
-
-var instrument = args[0];
-
 const config = require('../../config.js')
-
 const UDP = require('dgram')
 const musician = UDP.createSocket('udp4')
+const { v4: uuidv4 } = require('uuid')
 
 const port = config.PROTOCOL_PORT;
 const multicast_address = config.PROTOCOL_MULTICAST_ADDRESS;
 
+var args = process.argv.slice(2);
+console.log(args);
+
+
+/*
 function makeNoise(instrument) {
     switch (instrument) {
         case 'flute':
@@ -31,17 +25,37 @@ function makeNoise(instrument) {
             return "pouet"
     }
 }
+*/
 
-const packet = Buffer.from(makeNoise(instrument))
+const instruments = {
+    flute: "trulu",
+    drum: "boum-boum",
+    piano: "ti-ta-ti",
+    violin: "gzi-gzi",
+    trumpet: "pouet"
+}
+
+var instrument = instruments[args[0]];
+
+function uuid() {
+    return uuidv4();
+}
+
+const paquet = JSON.stringify({
+    uuid: uuid(),
+    instrument: instrument});
+
 
 function send() {
-    musician.send(packet, port, multicast_address, (err) => {
+    musician.send(paquet, port, multicast_address, (err) => {
         if (err) {
             console.error('Failed to send packet !!')
         } else {
-            console.log('Packet send !!')
+            console.log('Sending paquet: ' + paquet + ' via port ' + musician.address().port);
         }
     })
 }
 
 const timeId = setInterval(send, 1000)
+
+
