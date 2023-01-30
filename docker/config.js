b3e7ccfee60b2254283aa7ced91d8cbb307f7788
Céline Roger <roger.celine93@gmail.com>
2023-01-30 11:29:53 +0100

    modif musician

--- docker/config.js
+++ docker/config.js
@@ -2,6 +2,20 @@
  * Our application protocol specifies the following default multicast address and port
  */
 
-exports.PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
-exports.PROTOCOL_PORT = 9907;
-exports.PROTOCOL_MUSICIAN_TIMEOUT = 5000;
+const PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
+const PROTOCOL_PORT = 9907;
+const PROTOCOL_MUSICIAN_TIMEOUT = 5000;
+
+const INSTRUMENTS = {
+    flute: "trulu",
+    drum: "boum-boum",
+    piano: "ti-ta-ti",
+    violin: "gzi-gzi",
+    trumpet: "pouet"
+}
+
+module.exports = {
+    address: PROTOCOL_MULTICAST_ADDRESS,
+    port: PROTOCOL_PORT,
+    instruments: INSTRUMENTS
+}
---
+++ docker/image-auditor/src/musician.js
@@ -0,0 +1,9 @@
+class Musician {
+    constructor(uuid, instrument, activeSince) {
+        this.uuid = uuid;
+        this.instrment = instrument;
+        this.activeSince = activeSince;
+    }
+}
+
+module.exports = { Musician };
